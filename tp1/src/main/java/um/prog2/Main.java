517d36696ea43be0b88ab3af6a62104be627a131
meschinidino <francodmsoloing@gmail.com>
2025-04-06 12:24:06 -0300

    gestion de vehiculos con arraylist

--- tp1/src/main/java/um/prog2/Main.java
+++ tp1/src/main/java/um/prog2/Main.java
@@ -6,21 +6,44 @@ import um.prog2.vehiculo.Camion;
 import um.prog2.vehiculo.Vehiculo;
 import um.prog2.vehiculo.VehiculoPrinter;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        VehiculoPrinter vehiculoPrinter = new VehiculoPrinter();
+        ArrayList<Vehiculo> vehiculos = getVehiculos();
+
+        for (Vehiculo vehiculo : vehiculos) {
+            switch (vehiculo) {
+                case Auto auto -> {
+                    System.out.println("info para el auto");
+                    VehiculoPrinter.mostrarInfoVehiculo(auto);
+                }
+                case Camion camion -> {
+                    System.out.println("info para el camion");
+                    VehiculoPrinter.mostrarInfoVehiculo(camion);
+                }
+                default -> {
+                    System.out.println("info para el vehiculo");
+                    VehiculoPrinter.mostrarInfoVehiculo(vehiculo);
+                }
+            }
+        }
+    }
+
+    private static ArrayList<Vehiculo> getVehiculos() {
         Vehiculo vehiculo1 = new Vehiculo(1.12312, 2009, "hyundai", "GSC 123");
-        System.out.println("info para el vehiculo 1");
-        VehiculoPrinter.mostrarInfoVehiculo(vehiculo1);
         Vehiculo vehiculo2 = new Vehiculo(2.12312, 2015, "hyundai", "HTD 234");
-        System.out.println("info para el vehiculo 2");
-        VehiculoPrinter.mostrarInfoVehiculo(vehiculo2);
         Vehiculo vehiculo3 = new Vehiculo(3.12312, 2019, "hyundai", "AD 123 AA");
-        System.out.println("info para el vehiculo 3");
-        VehiculoPrinter.mostrarInfoVehiculo(vehiculo3);
         Camion vehiculo4 = new Camion(4.12312, 2020, "hyundai", "AE 123 AA", true);
-        VehiculoPrinter.mostrarInfoVehiculo(vehiculo4);
         Auto vehiculo5 = new Auto(5.12312, 2021, "hyundai", "AF 123 AA", 4);
-        VehiculoPrinter.mostrarInfoVehiculo(vehiculo5);
+
+
+        ArrayList<Vehiculo> vehiculos = new ArrayList<Vehiculo>();
+        vehiculos.add(vehiculo1);
+        vehiculos.add(vehiculo2);
+        vehiculos.add(vehiculo3);
+        vehiculos.add(vehiculo4);
+        vehiculos.add(vehiculo5);
+        return vehiculos;
     }
 }
