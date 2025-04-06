920524d6e5001ddf12d0da4d9eadfebed618f1d6
meschinidino <francodmsoloing@gmail.com>
2025-04-06 11:57:48 -0300

    creada la clase Auto con atributo cantidadPasajeros

--- tp1/src/main/java/um/prog2/vehiculo/VehiculoPrinter.java
+++ tp1/src/main/java/um/prog2/vehiculo/VehiculoPrinter.java
@@ -9,4 +9,8 @@ public class VehiculoPrinter {
     public static void mostrarInfoVehiculo(Camion camion) {
         System.out.printf("Marca: %s \nPatente: %s\n Anio: %d\n capacidadCargaKg: %f\n tieneAcoplado: %b\n" , camion.getMarca(), camion.getPatente(), camion.getAnio(), camion.getCapacidadCargaKg(), camion.isTieneAcoplado());
     }
+
+    public static void mostrarInfoVehiculo(Auto auto){
+        System.out.printf("Marca: %s \nPatente: %s\n Anio: %d\n capacidadCargaKg: %f\n cantidadPasajeros: %d\n" , auto.getMarca(), auto.getPatente(), auto.getAnio(), auto.getCapacidadCargaKg(), auto.getCantidadPasajeros());
+    }
 }
